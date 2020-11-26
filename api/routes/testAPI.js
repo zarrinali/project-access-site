8fc29145f0355e888c85170dcec63a4ca5423542
Zarrin <zali11@fordham.edu>
2020-11-26 14:33:19 -0500

    express and react

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,8 @@
-import { Router } from "express";
-var router = Router();
+var express = require("express");
+var router = express.Router();
 
 router.get("/", function(req, res, next) {
     res.send("API is working properly");
 });
 
-export default router;
+module.exports = router;
