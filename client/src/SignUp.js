e261037e04b55cab997fde30423d4ded36afe3a7
tsmorita3 <tsubasamorita3@gmail.com>
2020-12-22 15:49:22 -0500

    shifted Landing Pages to be component based

--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -1,95 +1,9 @@
 import React from 'react';
-import styles from './SignUp.module.css';
-import logo from './assets/images/austria.png';
-import key from './assets/images/logo_key_white.png';
-import axios from 'axios';
+import Content from './components/LandingPages/SignUpContent';
 
 class SignUp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-
-    const user = {
-      email: this.state.email,
-      password: this.state.password,
-    };
-
-    axios
-      .post('http://localhost:9000/auth/signup', { user })
-      .then((res) => {
-        console.log(res);
-        return res;
-      })
-      .then((res) => {
-        return (
-          <div>
-            {res.data.user.record_id}
-            {res.data.user.email}
-            {res.data.message}
-          </div>
-        );
-      })
-      .catch((err) => console.log(err));
-  }
-
   render() {
-    return (
-      <div className={styles.SignUp}>
-        <main>
-          <a href="/" className={styles.logo_link}>
-            <img src={logo} className={styles.logo} alt="logo" />
-          </a>
-          <section className={styles.card}>
-            <h1>Sign Up</h1>
-            <img src={key} className={styles.key} alt="key" />
-            <form onSubmit={this.handleSubmit}>
-              <label for="email">Email</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-                required
-              />
-              <label for="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                value={this.state.password}
-                onChange={this.handleChange}
-                required
-              />
-              <button type="submit">Register</button>
-            </form>
-            <p className={styles.sign_up}>
-              Already have an account? <a href="/login">Login</a>
-            </p>
-          </section>
-        </main>
-      </div>
-    );
+    return <Content />;
   }
 }
 
---
+++ client/src/components/Register/Register.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './Register.css';
+import Carousel from '../Carousel/Carousel';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import 'react-multi-carousel/lib/styles.css';
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      name: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="testimonials_signup">
+        <div className="testimonials">
+          <FontAwesomeIcon icon={faQuoteRight} className="quote_right" />
+          <FontAwesomeIcon icon={faQuoteLeft} className="quote_left" />
+          <Carousel />
+        </div>
+        <div className="signup">
+          <h3>Sign Up Here for Help with Your Applications</h3>
+          <p>
+            To find out more about how Project Access can help you, we only ask for your first name
+            and your email address to send you more information. Just fill out the form below and we
+            will get in touch!
+          </p>
+          <form onSubmit={this.handleSubmit}>
+            <label for="name">First and Last Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              required
+            />
+            <label for="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              required
+            />
+            <button type="submit">Register</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
