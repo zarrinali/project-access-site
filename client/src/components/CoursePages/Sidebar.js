d9c7d881ce8832170c877f695a30aa119154298e
tsmorita3 <tsubasamorita3@gmail.com>
2020-12-23 17:34:23 -0500

    added Dashboard
    also fixed some styling conflicts

--- client/src/components/CoursePages/Sidebar.js
+++ client/src/components/CoursePages/Sidebar.js
@@ -2,38 +2,11 @@ import './Sidebar.css';
 import React from 'react';
 
 function Sidebar() {
-<<<<<<< HEAD
-    return (
-        <div className="side-bar">
-            <p id="sidebar-title">
-                <a href="/prebootcampcourse"> Pre-Bootcamp Course </a>
-            </p>
-            <ul className="navigation">
-                <li>
-                    <a href="/module1">Module One</a>
-                </li>
-                <li>
-                    <a href="/module2">Module Two</a>
-                </li>
-                <li>
-                    <a href="/module3">Module Three</a>
-                </li>
-                <li>
-                    <a href="/module4">Module Four</a>
-                </li>
-                <li>
-                    <a href="/module5">Module Five</a>
-                </li>
-                <li>
-                    <a href="/module6">Module Six</a>
-                </li>
-            </ul>
-        </div>
-    )
-=======
   return (
     <div className="side-bar">
-      <p>Pre-Bootcamp Course</p>
+      <p id="sidebar-title">
+        <a href="/prebootcampcourse"> Pre-Bootcamp Course </a>
+      </p>
       <ul className="navigation">
         <li>
           <a href="/module1">Module One</a>
@@ -56,7 +29,6 @@ function Sidebar() {
       </ul>
     </div>
   );
->>>>>>> e261037e04b55cab997fde30423d4ded36afe3a7
 }
 
 export default Sidebar;
